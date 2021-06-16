21c0030cce05e890715c660750612f0f6ff97eb1
John Annyskier <john-ao@hotmail.com>
2021-06-16 19:42:56 -0300

    atualzação do C#

---
+++ C# dotnet/SupplyControl/SupplyControl/model/supply/SolderPaste.cs
@@ -0,0 +1,17 @@
+﻿using SupplyControl.interfaces;
+
+namespace SupplyControl.model.supply
+{
+    public class SolderPaste : SupplyBase, ISupply
+    {
+        public SolderPaste(string code) : base(code)
+        {
+
+        }
+
+        public string GetCode()
+        {
+            return base.code;
+        }
+    }
+}
---
+++ C# dotnet/SupplyControl/SupplyControl/model/supply/SupplyBase.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SupplyControl.model.supply
+{
+    public abstract class SupplyBase
+    {
+        protected string code;
+
+        public SupplyBase(string code) 
+        {
+            this.code = code;
+        }
+    }
+}
