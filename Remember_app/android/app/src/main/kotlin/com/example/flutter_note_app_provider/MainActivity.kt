ba3a3962a2e142437b9d97f4f25564f797b31bd0
John Annyskier <43702301+johnannysker@users.noreply.github.com>
2021-09-04 14:53:33 -0300

    Excluindo arquivos duplicados

--- Remember_app/android/app/src/main/kotlin/com/example/flutter_note_app_provider/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_note_app_provider
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Remember_app/lib/models/Tarefa.dart
+++
@@ -1,6 +0,0 @@
-class Tarefas{
-  String titulo;
-  String descricao;
-
-  Tarefas(this.titulo,this.descricao);
-}
